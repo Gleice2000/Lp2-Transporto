8019eead2d9a83443b5ee0acf2b82e9e40bd2baf
Gleiceara Alves de Azevedo <gleiceara.alves@alunos.ifsuldeminas.edu.br>
2023-03-27 19:48:14 -0300

    Update Teste.java

--- Trasporto/src/view/Teste.java
+++ Trasporto/src/view/Teste.java
@@ -8,9 +8,9 @@ import model.Viagem;
 public class Teste {
     public static void main(String[] args) {
         Carreta carreta1 = new Carreta("Carreta ct-7000");
-        Caminhoneiro c1 = new Caminhoneiro("João Alves", "CNPJ do Caminhoneiro", "CPF do Caminhoneiro", 1575.0);
+        Caminhoneiro c1 = new Caminhoneiro("João Alves", 1575.0);
         Viagem viagem1 = new Viagem("São Paulo", 4.5, 350);
-        Cliente cliente1 = new Cliente("Pedro", c1, viagem1, carreta1);
+        Cliente cliente1 = new Cliente("Pedro");
 
         cliente1.visualizarViagem();
     }
